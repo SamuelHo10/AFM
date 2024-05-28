1c423e90c586a2ad66e6b5a73785094680e7dfa5
Samuel Ho <samuel.is.pathetic@gmail.com>
2024-05-28 17:31:10 -0400

    updated code once more

--- chain_fit.py
+++ chain_fit.py
@@ -2,53 +2,72 @@ import pandas as pd
 from pathlib import Path
 from tkinter import filedialog
 import units
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-
-OUTPUT_FILE = "general_output.csv"
-min_position_threshold = 300e-9
+def histogram(values, x_label, y_label):
+    """
+    This function creates a bar graph from the given data.
+    """
+    w = 20
+    plt.hist(values, bins=np.arange(min(values), max(values) + w, w))
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
 
 def analyse_data(file_path):
     
     data = pd.read_csv(file_path, sep="\t")
 
-    print(data.columns)
+    # print(data.columns)
     
     data = units.change_column_prefix(data, "Bending Length [m]", "p")
     data = units.change_column_prefix(data, "Contour Length [m]", "n")
     data = units.change_column_prefix(data, "Residual RMS [N]", "p")
+    data = units.change_column_prefix(data, "Breaking Force [N]", "p")
 
     data = data[(data["Bending Length [pm]"] < 4000) & (data["Bending Length [pm]"] > 20) & (data["Contour Length [nm]"] < 5000) & (data["Contour Length [nm]"] > 300) & (data["Residual RMS [pN]"] < 25)]
 
-    # no_interaction = ((data["Fitted Segment Count"] == 0) | ((data["Fitted Segment Count"] == 1) & (data["Minimum Position [m]"] < min_position_threshold))).sum()
-    # specific = ((data["Fitted Segment Count"] == 1) & (data["Minimum Position [m]"] >= min_position_threshold)).sum()
-    # non_specific = (data["Fitted Segment Count"] > 1).sum()
-    # total = no_interaction + specific + non_specific
-    
-    
-    # el = [Path(file_path).stem, no_interaction, specific, non_specific, no_interaction/total, specific/total, non_specific/total]
-    
-    # file_names = [x[0] for x in output_list]
-    
-    # if el[0] not in file_names:
-    #     output_list.append(el)
-    
-    # data = data[(data["Fitted Segment Count"] > 1) | ((data["Fitted Segment Count"] == 1) & (data["Minimum Position [m]"] >= min_position_threshold))]
-
-    # data = data.drop(columns=['Filename', 'Position Index', 'X Position', 'Y Position','Baseline Offset [N]', 'Baseline Slope [N/m]','Contact Point Offset [m]', 'Minimum Value [N]','Minimum Position [m]', 'Filter Group', 'Lower Bound [m]','Upper Bound [m]', 'Fitted Segment Count'])
-
     return data
+
+def max_length_dict(dictionary):
+    
+    max_length = 0
+    
+    for key in dictionary:
+        if len(dictionary[key]) > max_length:
+            max_length = len(dictionary[key])
+    
+    return max_length
 
 
 if __name__ == "__main__":
     
+    
+    breaking_forces = {}
+    
     # Prompt the user to select files
-    files = filedialog.askopenfilenames(filetypes=[("TSV files", "*.tsv")])
+    files = filedialog.askopenfilenames(filetypes=[("File Types:", "*.tsv *.txt")])
 
     for file in files:
 
         filtered_data = analyse_data(file)
+        
+        segments = Path(file).stem.split("-")
+        new_name = segments[0] + segments[2]
+        
+        breaking_forces[new_name] = filtered_data["Breaking Force [pN]"].tolist()
+        # histogram(filtered_data["Breaking Force [pN]"], "Breaking Force [pN]", "Frequency")
 
         # Save the filtered data to a new CSV file
-        filtered_data.to_csv(f"{Path(file).stem}_filtered.csv", index=False)
+        filtered_data.to_csv(f"chainfits\\{Path(file).stem}_filtered.csv", index=False)
+    
+    max_length = max_length_dict(breaking_forces)
+    for key in breaking_forces:
+        breaking_forces[key] = list(np.pad(breaking_forces[key], (0, max_length - len(breaking_forces[key])), 'constant', constant_values=(np.nan)))
+        print(len(breaking_forces[key]))
 
+    
+    breaking_forces_df = pd.DataFrame(breaking_forces)
+    breaking_forces_df.to_csv(f"breaking_forces.csv", index=False)
